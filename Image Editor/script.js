24b03168e65002131f3ec8fb520fc72e3d467086
Varun Pattar <varunpattar@gmail.com>
2019-08-25 20:06:04 +0530

    canvas in place of image

--- Image Editor/script.js
+++ Image Editor/script.js
@@ -1,22 +1,48 @@
-document.addEventListener('DOMContentLoaded',function(){
+document.addEventListener('DOMContentLoaded', function() {
 
     //Handling Image Input
 
-    
+
     var loadImage = document.getElementById('load');
 
-    function loadInput(event){
-        var ImageFile = event.target.files[0];
-        var ImageElement = document.getElementById('image');
-        ImageElement.setAttribute('src',URL.createObjectURL(ImageFile));
+    function loadInput(event) {
+        // var ImageFile = event.target.files[0];
+        // // var ImageElement = document.getElementById('image');
+        // // ImageElement.setAttribute('src', URL.createObjectURL(ImageFile));
+
+        // var canvas = document.getElementById("image");
+        // var ctx = canvas.getContext("2d");
+        // // var img = document.getElementById("scream");
+        // ctx.drawImage(ImageFile, 100, 100);
+
+        var imageLoader = document.getElementById('imageLoader');
+        imageLoader.addEventListener('change', handleImage, false);
+        var canvas = document.getElementById('imageCanvas');
+        var ctx = canvas.getContext('2d');
+
+
+        function handleImage(e) {
+            var reader = new FileReader();
+            reader.onload = function(event) {
+                var img = new Image();
+                img.onload = function() {
+                    canvas.width = img.width;
+                    canvas.height = img.height;
+                    ctx.drawImage(img, 0, 0);
+                }
+                img.src = event.target.result;
+            }
+            reader.readAsDataURL(e.target.files[0]);
+        }
+
 
     };
-    loadImage.onchange = loadInput;
+    // loadImage.onchange = loadInput;
 
     //Apply Slider Effect on Image
 
-    function changeSliderHandler(event){
-        Caman("#image",function renderCaman(){
+    function changeSliderHandler(event) {
+        Caman("#image", function renderCaman() {
             // this.revert(false);
             this[event.target.name](event.target.value).render();
         });
@@ -24,7 +50,7 @@ document.addEventListener('DOMContentLoaded',function(){
 
     //Handling changes in values
     var ranges = document.querySelectorAll('input[type="range"]');
-    ranges.forEach(function(range){
+    ranges.forEach(function(range) {
         range.onchange = changeSliderHandler;
     });
 
@@ -32,11 +58,11 @@ document.addEventListener('DOMContentLoaded',function(){
 
 
     //Reset Button Handler
-    function resetButtonHandler(event){
-        ranges.forEach(function(range){
+    function resetButtonHandler(event) {
+        ranges.forEach(function(range) {
             range.value = 0;
         });
-        Caman("#image",function(){
+        Caman("#image", function() {
             this.revert(true);
         });
     };
@@ -45,9 +71,10 @@ document.addEventListener('DOMContentLoaded',function(){
 
     //Save edited image
     var saveButton = document.getElementById('save');
-    function saveButtonHandler(event){
-        Caman('#image',function(){
-            this.render(function(){
+
+    function saveButtonHandler(event) {
+        Caman('#image', function() {
+            this.render(function() {
                 // this.save('EditedImage.png');
                 var image = this.toBase64();
                 this.save('png');
